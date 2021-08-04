2d4a194d179c47340880bd0331977273ca0cc794
Jeremy Taylor <jeremy.taylor.cs@gmail.com>
2021-08-04 15:53:07 -0700

    Add Quiz unit tests

---
+++ tests/quiz.ts
@@ -0,0 +1,58 @@
+jest.mock('session/quiz/question')
+import { nanoid } from 'nanoid'
+import { FillInFormat, Quiz } from 'session/quiz'
+import { Question } from 'session/quiz/question'
+
+describe('Quiz', () => {
+  // SUT
+  let quiz: Quiz
+
+  beforeEach(() => {
+    quiz = new Quiz()
+  })
+
+  it('should have currentIndex equals -1 before next question', () => {
+    expect(quiz.currentQuestionIndex).toBe(-1)
+  })
+
+  it('addQuestion should add question', () => {
+    let question = randomFillInQuestion()
+    quiz.addQuestion(question)
+    expect(quiz.questions.contains(question)).toBe(true)
+  })
+
+  it('questionAt should return same question as one in questions', () => {
+    quiz.addQuestion(randomFillInQuestion())
+    expect(quiz.questionAt(0)).toEqual(quiz.questions.get(0))
+  })
+
+  it('numQuestions should match questions.size', () => {
+    quiz.addQuestion(randomFillInQuestion())
+    expect(quiz.numQuestions).toBe(quiz.questions.size)
+  })
+
+  describe('advanceToNextQuestion', () => {
+    describe('with questions added', () => {
+      // Test data
+      let question: Question
+      beforeEach(() => {
+        question = randomFillInQuestion()
+        quiz.addQuestion(question)
+      })
+
+      it('should move index forward and start question', () => {
+        quiz.advanceToNextQuestion()
+        expect(quiz.currentQuestionIndex).toBe(0)
+        expect(quiz.currentQuestion).toBe(question)
+        expect(question.start).toHaveBeenCalled()
+      })
+    })
+  })
+})
+
+function randomFillInQuestion(): Question {
+  return new Question(nanoid(), {
+    type: FillInFormat,
+    answer: nanoid(),
+  })
+}
