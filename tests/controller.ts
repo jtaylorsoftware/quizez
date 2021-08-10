b4df6f405bcea85ffe04f65f98e0f51479ae7a85
Jeremy Taylor <jeremy.taylor.cs@gmail.com>
2021-08-10 00:54:41 -0700

    Add more controller handler tests

--- tests/controller.ts
+++ tests/controller.ts
@@ -2,7 +2,9 @@ jest.mock('socket.io')
 jest.mock('socket.io/dist/client')
 import SessionEvent from 'api/event'
 import { QuestionFormat } from 'api/question'
-import { ResponseStatus } from 'api/response'
+import { JoinSession } from 'api/request'
+import { EventResponse, ResponseStatus } from 'api/response'
+import { nanoid } from 'nanoid'
 import { SessionController } from 'server/controller'
 import { Session } from 'session'
 import { Namespace, Server, Socket } from 'socket.io'
@@ -16,15 +18,21 @@ describe('SessionController', () => {
   let server: Server
   let socket: Socket
   let session: Session
+  let socketId: string
 
   beforeEach(() => {
     server = new Server()
-
+    ;(server.in as jest.Mock).mockReturnValue({
+      socketsLeave: jest.fn(() => {}),
+    })
     socket = new Socket(
       new Namespace(server, 'namespace'),
       new Client(server, {}),
       {}
     )
+    socketId = nanoid()
+    // @ts-ignore
+    socket.id = socketId
 
     controller = new SessionController(server)
 
@@ -34,14 +42,105 @@ describe('SessionController', () => {
 
     // @ts-ignore
     controller.emit = jest.fn((target: string, response: any) => {})
-
-    session = new Session(socket.id)
     // @ts-ignore
-    controller._sessions = controller._sessions.set('session', session)
+    controller.emitExcept = jest.fn(
+      (target: string, except: string, response: any) => {}
+    )
+
+    session = new Session(socketId)
+    // @ts-ignore
+    controller._sessions = controller._sessions.set(session.id, session)
   })
 
   afterEach(() => {
     jest.useRealTimers()
+  })
+
+  describe('createSession', () => {
+    it('should not create session if there is no callback', () => {
+      controller.createSession(socket)()
+      expect(controller.sessions.size).toBe(1) // only the session added in setup
+    })
+  })
+
+  describe('addUserToSession', () => {
+    let userSocket: Socket
+    beforeEach(() => {
+      userSocket = new Socket(
+        new Namespace(server, 'namespace'),
+        new Client(server, {}),
+        {}
+      )
+      // @ts-ignore
+      userSocket.id = nanoid()
+    })
+    it('should not add user if no callback', () => {
+      const name = nanoid()
+      controller.addUserToSession(userSocket)(<JoinSession>{
+        id: session.id,
+        name,
+      })
+      expect(session.findUserByName(name)).toBeUndefined()
+    })
+
+    it('should add user if there is a callback', () => {
+      const name = nanoid()
+      controller.addUserToSession(userSocket)(
+        <JoinSession>{
+          id: session.id,
+          name,
+        },
+        () => {}
+      )
+      expect(session.findUserByName(name)).not.toBeUndefined()
+    })
+
+    it('should not add same socket twice', () => {
+      const name = nanoid()
+      const testName = nanoid()
+      let res!: EventResponse
+      controller.addUserToSession(userSocket)(
+        <JoinSession>{
+          id: session.id,
+          name,
+        },
+        () => {}
+      )
+      controller.addUserToSession(userSocket)(
+        <JoinSession>{
+          id: session.id,
+          name: testName,
+        },
+        (response: EventResponse) => {
+          res = response
+        }
+      )
+      expect(res.status).toBe(ResponseStatus.Failure)
+    })
+
+    it('should not add same username twice', () => {
+      const name = nanoid()
+      let res!: EventResponse
+      let testSocket = { ...userSocket }
+      testSocket.id = nanoid()
+      controller.addUserToSession(userSocket)(
+        <JoinSession>{
+          id: session.id,
+          name,
+        },
+        () => {}
+      )
+      controller.addUserToSession(testSocket as Socket)(
+        <JoinSession>{
+          id: session.id,
+          name,
+        },
+        (response: EventResponse) => {
+          res = response
+        }
+      )
+      expect(res.status).toBe(ResponseStatus.Failure)
+    })
   })
 
   describe('addQuestionToSession', () => {
@@ -49,12 +148,59 @@ describe('SessionController', () => {
       jest.useFakeTimers()
     })
 
+    it('should not add a Question if socket does not own session', () => {
+      const userSocket = new Socket(
+        new Namespace(server, 'namespace'),
+        new Client(server, {}),
+        {}
+      )
+      // @ts-ignore
+      userSocket.id = nanoid()
+
+      let res!: EventResponse
+      controller.addQuestionToSession(userSocket)(
+        {
+          session: session.id,
+          question: {
+            text: 'Question',
+            body: {
+              type: QuestionFormat.FillInFormat,
+              answers: [
+                { text: 'One', points: 200 },
+                { text: 'Two', points: 200 },
+              ],
+            },
+            timeLimit: 60,
+          },
+        },
+        (response: EventResponse) => {
+          res = response
+        }
+      )
+
+      expect(res.status).toBe(ResponseStatus.Failure)
+    })
+
+    it('should not add anything if there is no Question arg', () => {
+      let res!: EventResponse
+      controller.addQuestionToSession(socket)(
+        {
+          session: session.id,
+        },
+        (response: EventResponse) => {
+          res = response
+        }
+      )
+
+      expect(res.status).toBe(ResponseStatus.Failure)
+    })
+
     it('should add a Question with a timeout to emit QuestionEnded', () => {
       // Add question
       const handler = controller.addQuestionToSession(socket)
       handler(
         {
-          session: 'session',
+          session: session.id,
           question: {
             text: 'Question',
             body: {
@@ -88,6 +234,59 @@ describe('SessionController', () => {
           question: 0,
         },
       })
+    })
+  })
+
+  describe('removeUserFromSession', () => {
+    let userSocket: Socket
+    let username = 'user'
+    beforeEach((done) => {
+      userSocket = new Socket(
+        new Namespace(server, 'namespace'),
+        new Client(server, {}),
+        {}
+      )
+      // @ts-ignore
+      userSocket.id = nanoid()
+
+      controller.addUserToSession(userSocket)(
+        { id: session.id, name: username },
+        () => {
+          done()
+        }
+      )
+    })
+
+    it('should not remove user if socket does not own session', () => {
+      let res!: EventResponse
+      controller.removeUserFromSession(userSocket)(
+        {
+          session: session.id,
+          name: username,
+        },
+        (response: EventResponse) => {
+          res = response
+        }
+      )
+
+      expect(res.status).toBe(ResponseStatus.Failure)
+      expect(session.findUserByName(username)).not.toBeUndefined()
+    })
+
+    it('should actually remove user if socket owns session', () => {
+      let res!: EventResponse
+      controller.removeUserFromSession(socket)(
+        {
+          session: session.id,
+          name: username,
+        },
+        (response: EventResponse) => {
+          res = response
+        }
+      )
+
+      expect(res.status).toBe(ResponseStatus.Success)
+      expect(session.findUserByName(username)).toBeUndefined()
     })
   })
 })
